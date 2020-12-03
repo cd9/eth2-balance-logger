62775fca6fbccf5fad7b761029ad42542ec7d876
Colin <dalyco884@gmail.com>
2020-12-03 19:56:25 +0000

    use better name for gauge metric

--- balance-logger.py
+++ balance-logger.py
@@ -8,7 +8,7 @@ if len(argv) != 3:
 
 URL = "http://127.0.0.1:5052/eth/v1/beacon/states/head/validators/" + argv[1]
 UPDATE_INTERVAL = int(argv[2])
-balance_guage = Gauge('balance', 'Validator balance, in ETH')
+balance_guage = Gauge('eth_validator_balance', 'Validator balance, in ETH')
 start_http_server(9010)
 
 while (True):
