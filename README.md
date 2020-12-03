18e02e78309b448dd02200616acab888030d11f0
Colin <dalyco884@gmail.com>
2020-12-03 19:41:47 +0000

    add README.md and python script

---
+++ README.md
@@ -0,0 +1,3 @@
+## Ethereum 2.0 Beacon Node validator balance Prometheus Exporter
+
+Simple Python script to fetch an [Ethereum validator struct](https://ethereum.github.io/eth2.0-APIs/#/Beacon/getStateValidator) from a local beacon node HTTP server..
---
+++ balance-logger.py
@@ -0,0 +1,18 @@
+from sys import argv, exit
+from prometheus_client import start_http_server, Gauge
+from time import sleep
+import requests
+
+if len(argv) != 3:
+  exit("Usage: balance-logger.py VALIDATOR_ID UPDATE_INTERVAL")
+
+URL = "http://127.0.0.1:5052/eth/v1/beacon/states/head/validators/" + argv[1]
+UPDATE_INTERVAL = int(argv[2])
+balance_guage = Gauge('balance', 'Validator balance')
+start_http_server(9010)
+
+while (True):
+  response = requests.get(URL)
+  if response.ok:
+    balance_guage.set(float(response.json()["data"]["balance"]))
+  sleep(UPDATE_INTERVAL)
