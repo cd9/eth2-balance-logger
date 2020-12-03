ae534c22b4496c82d524cbb306d80e1acbb2ccfe
Colin <dalyco884@gmail.com>
2020-12-03 19:53:59 +0000

    convert from gwei to ETH

--- balance-logger.py
+++ balance-logger.py
@@ -8,11 +8,11 @@ if len(argv) != 3:
 
 URL = "http://127.0.0.1:5052/eth/v1/beacon/states/head/validators/" + argv[1]
 UPDATE_INTERVAL = int(argv[2])
-balance_guage = Gauge('balance', 'Validator balance')
+balance_guage = Gauge('balance', 'Validator balance, in ETH')
 start_http_server(9010)
 
 while (True):
   response = requests.get(URL)
   if response.ok:
-    balance_guage.set(float(response.json()["data"]["balance"]))
+    balance_guage.set(float(response.json()["data"]["balance"])/10**9)
   sleep(UPDATE_INTERVAL)
