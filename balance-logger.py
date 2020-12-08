be631b7cfdd84b4877161d565a66cf98b28a4d66
Colin <dalyco884@gmail.com>
2020-12-08 05:20:09 +0000

    add status label

--- balance-logger.py
+++ balance-logger.py
@@ -6,11 +6,16 @@ VALIDATOR_INDICES = [1,2,3] #REPLACE THIS WITH YOUR VALIDATOR INDEXES
 URL_PREFIX = "http://127.0.0.1:5052/eth/v1/beacon/states/finalized/validators/"
 UPDATE_INTERVAL = 15
 
-balance_guage = Gauge('validator_balance', 'Validator balance, in ETH', ['index'])
+balance_gauge = Gauge('validator_balance', 'Validator balance, in ETH', ['index', 'status'])
 start_http_server(9010)
 
 while (True):
   for validator_index in VALIDATOR_INDICES:
-    balance = get(URL_PREFIX+str(validator_index)).json()["data"]["balance"]
-    balance_guage.labels(index=validator_index).set(float(balance)/10**9)
+    response = get(URL_PREFIX+str(validator_index))
+    if not response.ok:
+      balance_gauge.labels(index=validator_index, status='unknown').set(0)
+      continue
+    balance = float(response.json()["data"]["balance"])/10**9
+    status = response.json()["data"]["status"]
+    balance_gauge.labels(index=validator_index, status=status).set(balance)
   sleep(UPDATE_INTERVAL)
